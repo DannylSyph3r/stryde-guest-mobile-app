5635ce834c1c65d0d7f9295b49c0bba7582654ef
DannylSyph3r <dopedanny001@gmail.com>
2024-09-05 00:55:07 +0100

    Launcher Icons and App Icons

--- lib/shared/app_graphics.dart
+++ lib/shared/app_graphics.dart
@@ -1,30 +1,17 @@
 abstract class AppGraphics {
-  //! Auth Assets
-  static const String authBG = 'auth_bg';
-
-  //! Onboarding Assets
-  static const String strydeLogo = 'stryde_logo';
-  static const String slide1 = 'slide1';
-  static const String slide2 = 'slide2';
-  static const String slide3 = 'slide3';
 
   //! Icon Assets
   static const String googleIcon = 'google';
   static const String appleIcon = 'apple';
-  static const String personIcon = 'individual';
-  static const String enterpriseIcon = 'enterprise';
   static const String camera = 'camera';
   static const String files = 'file';
   static const String cameraShotTaker = 'shot_taker';
-  static const String spray = 'spray';
-  static const String gas = 'gas';
-  static const String seat = 'car_seat';
-  static const String gear = 'gear';
 
   //! Kyc Assets
   static const String memeoji = 'pfp';
 
   //! Main App Assets
+  static const String strydeLogo = 'stryde_logo';
   static const carPl = 'car_pl_1';
   static const carPlOne = 'car_pl_1';
   static const carPlTwo = 'car_pl_2';
